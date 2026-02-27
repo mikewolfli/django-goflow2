8ddea7ab9900bbe37293f662a4a40d94f81c6ee3
mikewolfli <mikewolfli@163.com>
2026-02-27 14:38:13 +0800

    change name to django-goflow2 in pypi.org

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+from pathlib import Path
+
 from setuptools import setup
 
+
+BASE_DIR = Path(__file__).resolve().parent
+README_PATH = BASE_DIR / 'README.md'
+
 setup(
-    name='django-goflow',
+    name='django-goflow2',
     version='1.0',
     description='Fork from Eric Simorre\'s django-goflow, updated for modern Python and Django 4/5/6',
+    long_description=README_PATH.read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
     author='Mike Li',
     author_email='mikewolfli@163.com',
-    url='***',
+    url='https://github.com/mikewolfli/django-goflow',
+    project_urls={
+        'Source': 'https://github.com/mikewolfli/django-goflow',
+        'Tracker': 'https://github.com/mikewolfli/django-goflow/issues',
+    },
+    keywords='django workflow bpm goflow',
     packages=['goflow', 'goflow.apptools', 'goflow.runtime', 'goflow.graphics', 'goflow.graphics2', 'goflow.workflow', 'goflow.apptools.templatetags', 'goflow.runtime.templatetags', 'goflow.workflow.templatetags', 'goflow.graphics2.templatetags'],
     classifiers=[
         'Environment :: Web Environment',
